d5dae870e3eacded7e8c52ca698e1bf95eb11c1c
Jeff Ikediugwu <44656271+jikediugwu@users.noreply.github.com>
2018-12-20 16:09:40 -0500

    added comments...

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $("#add").click(function(){
        var taskToAdd = prompt ("Please Enter a task");
             $("#todo").prepend("<li>" + taskToAdd + "</li>");          
-    });
+    }); // end of click function for adding a task
 
    $("#delete").click(function() {
    $(".ui-selected").remove();
 
-});
-});
+}); // end of click function for deleting a task
+}); // end of ready 
